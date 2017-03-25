5e08e3ea7f372077e179708c192f885c3641a157
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-03-24 20:47:57 -0400

    Rename file and finish exercise

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,12 +1,21 @@
 function Hamming() {}
 
 Hamming.prototype.compute = function (one, two) {
-  let strand1 = one.length;
-  let strand2 = two.length;
-
-  if (strand1 !== strand2) {
-    throw new Error('DNA strands must be of equal length.')
+  if (one.length !== two.length) {
+    throw new Error('DNA strands must be of equal length.');
   }
-}
+
+  const split1 = one.split('');
+  const split2 = two.split('');
+  let diffHamm = 0;
+
+  for (let i = 0; i < one.length; i++) {
+    if (split1[i] !== split2[i]) {
+      diffHamm++;
+    }
+  }
+
+  return diffHamm;
+};
 
 module.exports = Hamming;
